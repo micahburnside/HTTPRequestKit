9c11cab13e8096735ecbacdb1ba8e829cc090bc9
Micah Burnside <micahburnside@protonmail.com>
2024-11-24 15:18:30 -0500

    Add HttpMethodType enum for defining HTTP request methods in HTTPRequestKit.

---
+++ Sources/HTTPRequestKit/HttpMethodType.swift
@@ -0,0 +1,18 @@
+//
+//  HttpMethodType.swift
+//  HTTPRequestKit
+//
+//  Created by Micah Burnside on 11/24/24.
+//
+
+import Foundation
+
+enum HttpMethodType: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+    case patch = "PATCH"
+    case head = "HEAD"
+    case options = "OPTIONS"
+}
